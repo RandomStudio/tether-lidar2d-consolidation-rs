54b0d59acc32b4616c205d1ae9f1b4bb71e03ca8
Stephen Buchanan <stephen.buchanan@random.studio>
2023-01-17 15:54:11 +0100

    usage descriptions

--- src/main.rs
+++ src/main.rs
@@ -60,33 +60,42 @@ const AUTOMASK_MIN_THRESHOLD_MARGIN: f64 = 50.;
 #[command(version, about, long_about = None)]
 
 struct Cli {
+    /// Where to load LIDAR device config
     #[arg(long="lidarConfigPath",default_value_t=String::from(CONFIG_FILE_PATH))]
     config_path: String,
 
     #[arg(long="agentType",default_value_t=String::from(AGENT_TYPE))]
     agent_type: String,
 
+    /// The IP address of the MQTT broker (server)
     #[arg(long = "tether.host", default_value_t=TETHER_HOST)]
     tether_host: std::net::IpAddr,
 
     #[arg(long = "loglevel",default_value_t=String::from("info"))]
     log_level: String,
 
+    /// Default min distance threshold (in mm) to use for unconfigured new devices
     #[arg(long = "defaultMinDistanceThreshold", default_value_t = MIN_DISTANCE_THRESHOLD)]
     default_min_distance_threshold: f64,
 
+    /// Max distance in mm to a point which can be included in a cluster
     #[arg(long = "clustering.neighbourhoodRadius", default_value_t = NEIGHBOURHOOD_RADIUS)]
     clustering_neighbourhood_radius: f64,
 
+    /// Min points count that constitutes a valid cluster
     #[arg(long = "clustering.minNeighbours", default_value_t = MIN_NEIGHBOURS)]
     clustering_min_neighbours: usize,
 
+    /// Exclude clusters above this size, in radius
     #[arg(long = "clustering.maxClusterSize", default_value_t = MAX_CLUSTER_SIZE)]
     clustering_max_cluster_size: f64,
 
+    /// By default, we drop tracking points (resolved clusters) that lie outside of the defined quad;
+    /// enable (use) this flag to include them
     #[arg(long = "perspectiveTransform.includeOutside")]
     transform_include_outside: bool,
 
+    /// Unless perspectiveTransform.includeOutside is enabled, drop tracking points outside range [0-margin,1+margin]
     #[arg(long = "perspectiveTransform.ignoreOutsideMargin", default_value_t=IGNORE_OUTSIDE_MARGIN)]
     transform_ignore_outside_margin: f64,
 
